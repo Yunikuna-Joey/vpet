77c4757edd458e77ea84e322fde006c277676ada
Joey <37638087+Yunikuna-Joey@users.noreply.github.com>
2023-09-15 13:14:11 -0700

    Loaded in test image

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
+# Python GUI interpreter 
 import tkinter as tk 
+
+# Utilize PIL (Python Imaging Library) to load pet images and animations 
+from PIL import Image, ImageTk
 
 # creating a window 
 root = tk.Tk()
@@ -9,10 +13,14 @@ canvas = tk.Canvas(root, width=200, height=200)
 canvas.pack()
 
 # find a way to load pet images and animations
+use_image = Image.open('Toadpicture.png')
+pet_image = ImageTk.PhotoImage(use_image)
 
+pet_label = tk.Label(canvas, image=pet_image)
+pet_label.image = pet_image
+pet_label.pack()
 
 # main loop to update pet state 
-
 def update_pet(): 
     # update pet's animations / behavior here  
     root.after(100, update_pet)
